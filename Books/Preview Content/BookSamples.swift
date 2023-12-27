de76afe5506fbc9b66f5490a1119fdadee14d0ea
Vladimir Fibe <motiw@icloud.com>
2023-12-27 23:12:32 +0600

    Preview

---
+++ Books/Preview Content/BookSamples.swift
@@ -0,0 +1,8 @@
+//
+//  BookSamples.swift
+//  Books
+//
+//  Created by Vladimir Fibe on 27.12.2023.
+//
+
+import Foundation
---
+++ Books/Preview Content/PreviewContainer.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewContainer.swift
+//  Books
+//
+//  Created by Vladimir Fibe on 27.12.2023.
+//
+
+import Foundation
