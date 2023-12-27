581fa4392445b78bdf66e1606b8d579e811aa644
Vladimir Fibe <motiw@icloud.com>
2023-12-27 06:12:44 +0600

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Vladimir Fibe on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
